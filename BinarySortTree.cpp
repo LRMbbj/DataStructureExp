9bfc3c679770a0a016a35f3e3df47a6e71e34751
LRMbbj <1325712737@qq.com>
2021-06-17 16:47:51 +0800

    Finish BST

--- BinarySortTree.cpp
+++ BinarySortTree.cpp
@@ -14,24 +14,36 @@ template<class T> class BST
 public:
 	BST(T r[],int n);
 	~BST();
+	BiNode<T> *Search(T key);
+	void Insert(T key);
+	bool DeleteBST(T key);
+private:
 	BiNode<T> *Search(BiNode<T> *&R, T key);
 	void Insert(BiNode<T> *&R, BiNode<T> *s);
 	void Delete(BiNode<T> * &R);
-	void DeleteBST(BiNode<T> *&R, T key);
-private:
+	bool DeleteBST(BiNode<T> *&R, T key);
+	void ReleaseNode(BiNode<T> *&R);
 	BiNode<T> *Root;
 };
 
 template<class T>
 BST<T>::BST(T r[],int n)
 {
-
+	Root = NULL;
+	for (int i = 0; i < n; i++)
+	{
+		BiNode<T> *node = new BiNode<T>();
+		node->data = r[i];
+		node->lch = NULL;
+		node->rch = NULL;
+		Insert(Root,node);
+	}
+	
 }
-
 template<class T>
 BST<T>::~BST()
 {
-
+	ReleaseNode(Root);
 }
 template<class T>
 BiNode<T>* BST<T>::Search(BiNode<T> *&R, T key)
@@ -56,15 +68,76 @@ void BST<T>::Insert(BiNode<T> *&R, BiNode<T> *s)
 template<class T>
 void BST<T>::Delete(BiNode<T> * &R)
 {
-	
+	BiNode<T> *q = R;
+	if (R->lch == NULL && R->rch == NULL)
+	{
+		R = NULL;
+		delete q;
+	}
+	else if(R->rch == NULL)
+	{
+		R = R->lch;
+		delete q;
+	}
+	else if(R->lch == NULL)
+	{
+		R = R->rch;
+		delete q;
+	}
+	else
+	{
+		R->data = R->rch->data;
+		Delete(R->rch);
+	}
 }
 template<class T>
-void BST<T>::DeleteBST(BiNode<T> *&R, T key)
+bool BST<T>::DeleteBST(BiNode<T> *&R, T key)
 {
+	if(R == NULL)
+		return false;
+	
+	if(R->data == key)
+	{
+		Delete(R);
+		return true;
+	}
+	else if(key < R->data)
+		return DeleteBST(R->lch, key);
+	else
+		return DeleteBST(R->rch, key);
+}
+template<class T>
+void BST<T>::ReleaseNode(BiNode<T> *&R)
+{
+	if(R == NULL)
+		return;
+	ReleaseNode(R->lch);
+	ReleaseNode(R->rch);
+	cout<<"Releasing Node:"<<R->data<<endl;
+	delete R;
 
+}
+
+template<class T>
+BiNode<T> * BST<T>::Search(T key)
+{
+	return Search(Root, key);
+}
+template<class T>
+void BST<T>::Insert(T key)
+{
+	Insert(Root, key);
+}
+template<class T>
+bool BST<T>::DeleteBST(T key)
+{
+	return DeleteBST(Root, key);
 }
 
 int main()
 {
-	
+	int a[] = {2,5,4,8,6,1,7};
+	BST<int> *bst = new BST<int>(a,7);
+	bst->DeleteBST(5);
+	delete bst;
 }
